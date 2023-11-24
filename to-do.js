0d7a36315f3f47d980b90822d3ba1c714585b276
AnaClaudiaSac <anaclaudiasacute75@gmail.com>
2023-11-23 23:02:20 -0300

    feat: implement add function

--- to-do.js
+++ to-do.js
@@ -14,9 +14,39 @@ const task3 = {
 };
 
 let taskList = []
-taskList.push(task1)
-taskList.push(task2)
-taskList.push(task3)
+
+
+function addTask(task) {
+  taskList.push(task);
+};
+
+addTask(task1);
+addTask(task2);
+addTask(task3);
+
+addTask({
+  name: 'estudar',
+  isDone: false,
+});
+
+const task4 = {
+  name: 'praticar a lição',
+  isDone: false,
+};
+
+const task5 = {
+  name: 'assistir aula',
+  isDone: false,
+};
+
+
+addTask(task5)
+addTask(task4)
 
 
 
+
+
+
+
+console.log(taskList)
