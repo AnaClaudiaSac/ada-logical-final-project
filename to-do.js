64fef7cc1097115a3bfc9d19c815b3a55a1f7f72
AnaClaudiaSac <anaclaudiasacute75@gmail.com>
2023-11-23 22:14:21 -0300

    create initial task list

---
+++ to-do.js
@@ -0,0 +1,22 @@
+const task1 = {
+  name: 'fazer café',
+  isDone: false,
+};
+
+const task2 = {
+  name: 'lavar a louça',
+  isDone: false,
+};
+
+const task3 = {
+  name: 'guardar a louça',
+  isDone: false,
+};
+
+let taskList = []
+taskList.push(task1)
+taskList.push(task2)
+taskList.push(task3)
+
+
+
